8f8e990997e4af633b9522b681c8cb683c6cc411
vuvuvuvvv <nguyenvu260502@gmail.com>
2024-10-09 15:00:54 +0700

    fix: input cannot write decimal num

--- src/components/tinh-sai-so-form.tsx
+++ src/components/tinh-sai-so-form.tsx
@@ -29,8 +29,8 @@ export default function DNBT30TinhSaiSoForm({ className, formValue, readOnly = f
     useEffect(() => {
         setV1(formValue.V1.toString());
         setV2(formValue.V2.toString());
-        setVc1(formValue.Vc1.toString());
-        setVc2(formValue.Vc2.toString());
+        setVc1(Vc1 || formValue.Vc1.toString());
+        setVc2(Vc2 || formValue.Vc2.toString());
         setTdh(formValue.Tdh.toString());
         setTc(formValue.Tc.toString());
     }, [formValue]);
@@ -54,10 +54,7 @@ export default function DNBT30TinhSaiSoForm({ className, formValue, readOnly = f
             if (numericInputTimeout) {
                 clearTimeout(numericInputTimeout);
             }
-            const timeout = setTimeout(() => {
-                onFormChange(field, numericValue);
-            }
-                , 700);
+            const timeout = setTimeout(() => onFormChange(field, numericValue), 700);
             setNumericInputTimeout(timeout);
         };
     };
