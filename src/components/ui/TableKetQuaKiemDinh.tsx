da2697c4857b5b0aaab72bdb3ac70c07bc143295
vuvuvuvvv <nguyenvu260502@gmail.com>
2025-05-23 13:25:29 +0700

    signed off by vuvuvuvvv

--- src/components/ui/TableKetQuaKiemDinh.tsx
+++ src/components/ui/TableKetQuaKiemDinh.tsx
@@ -1,50 +1,25 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import dynamic from "next/dynamic";
 const ToggleSwitchButton = dynamic(() => import('@/components/ui/ToggleSwitchButton'));
-import { Modal, Button, Form } from 'react-bootstrap';
 
 import '@styles/scss/ui/modal-kiem-dinh.scss';
 import c_tbKD from "@styles/scss/components/table-kiem-dinh.module.scss";
-import Swal from "sweetalert2";
-
 import { useDongHoList } from '@/context/ListDongHoContext';
-import InputField from './InputFieldTBDHInfo';
 import { useKiemDinh } from '@/context/KiemDinhContext';
-import { DuLieuCacLanChay, DuLieuChayDongHo, DuLieuMotLanChay } from '@lib/types';
-import { getHieuSaiSo, getSaiSoDongHo, isDongHoDatTieuChuan } from '@lib/system-function';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAdd, faCogs, faRemove, faSave } from '@fortawesome/free-solid-svg-icons';
-import ModalInputSoLuongDongHo from './ModalInputSoLuongDongHo';
-import { ACCESS_LINKS } from '@lib/system-constant';
+import { isDongHoDatTieuChuan } from '@lib/system-function';
 interface ModalKiemDinhProps {
-    show: boolean;
+    activeLL?: ActiveLL;
     isEditing?: boolean;
-    handleClose: () => void;
 }
 
 type ActiveLL = { title: string; value: string } | null;
 
-export default function TableKetQuaKiemDinh({ show, isEditing = false, handleClose }: ModalKiemDinhProps) {
-    const [showModal, setShowModal] = useState(show);
-    const { luuLuong, getDuLieuChayCuaLuuLuong } = useKiemDinh();
+export default function TableKetQuaKiemDinh({ activeLL, isEditing = false }: ModalKiemDinhProps) {
+
+    const { luuLuong } = useKiemDinh();
     const {
         dongHoList,
-        savedDongHoList,
     } = useDongHoList();
-    const [isUsingBinhChuan, setUsingBinhChuan] = useState(false);
-    const [activeLL, setActiveLL] = useState<ActiveLL | null>(null);
-
-    useEffect(() => {
-        setShowModal(show);
-    }, [show]);
-
-    const handleChange = (
-        soLan: number,
-        indexDongHo: number,
-        newDL: DuLieuMotLanChay,
-        field: keyof DuLieuMotLanChay
-    ) => {
-    }
 
     return (
         luuLuong && <>
@@ -59,9 +34,9 @@ export default function TableKetQuaKiemDinh({ show, isEditing = false, handleClo
                             <th colSpan={2}>Xem trước</th>
                         </tr>
                         <tr>
-                            <th>{luuLuong.q3Orn?.title ?? <>Q<sub>III</sub></>}{luuLuong.q3Orn?.value ? " = " + luuLuong.q3Orn?.title : ""}</th>
-                            <th>{luuLuong.q2Ort?.title ?? <>Q<sub>II</sub></>}{luuLuong.q2Ort?.value ? " = " + luuLuong.q2Ort?.title : ""}</th>
-                            <th>{luuLuong.q1Ormin?.title ?? <>Q<sub>I</sub></>}{luuLuong.q1Ormin?.value ? " = " + luuLuong.q1Ormin?.title : ""}</th>
+                            <th>{luuLuong.q3Orn?.title ?? <>Q<sub>III</sub></>}{luuLuong.q3Orn?.value ? " = " + luuLuong.q3Orn?.value : ""}</th>
+                            <th>{luuLuong.q2Ort?.title ?? <>Q<sub>II</sub></>}{luuLuong.q2Ort?.value ? " = " + luuLuong.q2Ort?.value : ""}</th>
+                            <th>{luuLuong.q1Ormin?.title ?? <>Q<sub>I</sub></>}{luuLuong.q1Ormin?.value ? " = " + luuLuong.q1Ormin?.value : ""}</th>
                             <th>Biên bản</th>
                             <th>Giấy chứng nhận</th>
                         </tr>
@@ -87,13 +62,13 @@ export default function TableKetQuaKiemDinh({ show, isEditing = false, handleClo
                                             <span>ĐH {dongHo.index}</span>
                                         </td>
                                         <td>
-                                            {hieu_sai_so[0].hss}
+                                            {hieu_sai_so[0].hss ?? "-"}
                                         </td>
                                         <td>
-                                            {hieu_sai_so[1].hss}
+                                            {hieu_sai_so[1].hss ?? "-"}
                                         </td>
                                         <td>
-                                            {hieu_sai_so[2].hss}
+                                            {hieu_sai_so[2].hss ?? "-"}
                                         </td>
                                         <td>
                                             {ket_qua == null ? "Chưa kiểm định" : (ket_qua ? "Đạt" : "Không đạt")}
